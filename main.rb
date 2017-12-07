8ef1e51b003aef90cf832f4b4c031fcb02ff396d
Martin Cavoj <martin.cavoj@renuo.ch>
2017-12-07 14:50:32 +0100

    3

--- main.rb
+++ main.rb
@@ -8,11 +8,12 @@ end
 
 post '/decide' do
   content_type :json
+  # {"elevator"=>{"capacity"=>6, "current_floor"=>13, "target_floors"=>[14, 14, 14, 14, 14, 14], "statistics"=>120}, "floors"=>[{"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>true, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>true, "down"=>true}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>true}}, {"panel"=>{"up"=>false, "down"=>false}}, {"panel"=>{"up"=>false, "down"=>true}}]}
   params_json = JSON.parse(request.body.read)
 
   # Do the calculations
   puts "Got: \n#{params_json}"
-  target_level = rand(15)
+  target_level = (params_json['elevator']['current_floor'] += 1) % 15
 
   target_level.to_json
 end
